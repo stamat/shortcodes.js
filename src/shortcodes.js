0e166026a9064e6ef8d0bc2c9d7f10dc166f9399
Nikola Stamatovic <stamatmail@gmail.com>
2023-08-14 13:18:09 +0200

    fix passing the class names 🤦‍♂️

--- src/shortcodes.js
+++ src/shortcodes.js
@@ -444,7 +444,8 @@ Shortcodes.prototype.register = function(shortcode_name, descriptor) {
 		const template = self.construct(shortcode_obj)
 
 		if (template) {
-			if (shortcode_obj.classes.length) template.classList.add(shortcode_obj.classes.join(' '))
+			console.log(shortcode_obj.classes)
+			if (shortcode_obj.classes.length) template.classList.add(...shortcode_obj.classes)
 			css(template, shortcode_obj.css)
 			template.classList.add('shortcode-js')
 		}
